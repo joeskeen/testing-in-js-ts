7d26cfe4086d9aaef8c661fc02265fceda94abfa
Joe Skeen <joe.skeen@healthcatalyst.com>
2022-07-21 14:07:13 -0600

    end of the session

--- index.spec.ts
+++ index.spec.ts
@@ -1,3 +1,38 @@
-describe('thing', () => {
-  it('should', () => expect(true).toBe(true));
+describe(`Joe's Pizza Shop ordering`, () => {
+  let pizzaShop;
+  beforeEach(() => {
+    // TODO: instantiate the pizza shop
+  })
+  describe('when you order', () => {
+    let pizza;
+    describe('a classic', () => {
+      describe('cheese', () => {
+        beforeEach(() => {
+          pizza = {
+            ingredients: ['cheese']
+          }
+
+          // pizza = pizzaShop.orderClassic('cheese');
+        })
+        it('should only have cheese', () => {
+          expect(pizza.ingredients).toEqual(['cheese']);
+        });
+      });
+      describe('pepperoni', () => {
+        beforeEach(() => {
+          pizza = {ingredients: ['sausage', 'cheese', 'pepperoni']}
+        })
+        it('should have two ingredients', () => expect(pizza.ingredients.length).toBe(2));
+        it('should have cheese', () => expect(pizza.ingredients).toContain('cheese'));
+        it('should have pepperoni', () => expect(pizza.ingredients).toContain('pepperoni'));
+        
+        it('should only have pepperoni and cheese', () => {
+          expect(pizza.ingredients).toContain('pepperoni');
+          expect(pizza.ingredients).toContain('cheese');
+          expect(pizza.ingredients.length).toBe(2);
+        });
+      });
+      // ...
+    });
+  });
 });
